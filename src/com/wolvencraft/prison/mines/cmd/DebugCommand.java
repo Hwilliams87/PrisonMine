989fd2af438992e2cac1608e0372f2aa17cf0208
Kirill Bondarchuk <bondarchuk.kirill@gmail.com>
2012-12-14 12:56:43 -0800

    Feature: Debug command that unloads a mine from memory

--- src/com/wolvencraft/prison/mines/cmd/DebugCommand.java
+++ src/com/wolvencraft/prison/mines/cmd/DebugCommand.java
@@ -1,5 +1,7 @@
 package com.wolvencraft.prison.mines.cmd;
 
+import java.util.List;
+
 import org.bukkit.entity.Player;
 
 import com.wolvencraft.prison.PrisonSuite;
@@ -40,6 +42,12 @@ public class DebugCommand implements BaseCommand {
 			player.teleport(curMine.getRegion().getMaximum());
 			Message.sendCustom("DEBUG", "Teleported to: " + curMine.getId());
 			return true;
+		} else if(args[0].equalsIgnoreCase("unload")){
+			List<Mine> mines = PrisonMine.getMines();
+			mines.remove(mines.indexOf(args[1]));
+			PrisonMine.setMines(mines);
+			Message.sendCustom("DEBUG", "Unloaded " + args[1] + " from memory");
+			return true;
 		} else {
 			Message.sendError(PrisonMine.getLanguage().ERROR_COMMAND);
 			return false;
@@ -51,6 +59,7 @@ public class DebugCommand implements BaseCommand {
 		Message.formatHeader(20, "Debug");
 		Message.formatHelp("setregion", "<id>", "Sets the reset region of a mine to the one specified");
 		Message.formatHelp("tp", "<id>", "Teleports the sender to the specified mine");
+		Message.formatHelp("unload", "<id>", "Unloads the mine from the memory. The mine will be loaded back on server restart");
 	}
 
 	@Override
