01e4e6c6e6f59b62c7817d6854184184bded1870
Kirill Bondarchuk <bondarchuk.kirill@gmail.com>
2013-01-21 06:33:43 -0800

    Dev: New config option: manual mine reset causes children mines to reset

--- src/com/wolvencraft/prison/mines/settings/Settings.java
+++ src/com/wolvencraft/prison/mines/settings/Settings.java
@@ -12,6 +12,7 @@ public class Settings extends com.wolvencraft.prison.settings.Settings {
 	public final boolean PLAYERS_TP_ON_RESET;
 	public final boolean RESET_FORCE_TIMER_UPDATE;
 	public final boolean RESET_ALL_MINES_ON_STARTUP;
+	public final boolean RESET_TRIGGERS_CHILDREN_RESETS;
 	public final List<String> BANNEDNAMES;
 	public final Material[] TOOLS = {Material.WOOD_PICKAXE, Material.STONE_PICKAXE, Material.IRON_PICKAXE, Material.GOLD_PICKAXE, Material.DIAMOND_PICKAXE};
 	public final int DEFAULTTIME;
@@ -21,6 +22,7 @@ public class Settings extends com.wolvencraft.prison.settings.Settings {
 		PLAYERS_TP_ON_RESET = plugin.getConfig().getBoolean("players.teleport-players-out-of-the-mine");
 		RESET_FORCE_TIMER_UPDATE = plugin.getConfig().getBoolean("reset.force-reset-timer-on-mine-reset");
 		RESET_ALL_MINES_ON_STARTUP = plugin.getConfig().getBoolean("reset.reset-all-mines-on-startup");
+		RESET_TRIGGERS_CHILDREN_RESETS = plugin.getConfig().getBoolean("reset.manual-resets-children");
 		BANNEDNAMES = new ArrayList<String>();
 		for(CommandManager cmd : CommandManager.values()) {
 			for(String alias : cmd.getLocalAlias()) {
