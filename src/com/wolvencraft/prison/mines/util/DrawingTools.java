4eae4d39db328dc9c35d0965819e10fa514807b3
Kirill Bondarchuk <bondarchuk.kirill@gmail.com>
2013-02-07 21:26:00 -0800

    Bug fix: Incorrect format

--- src/com/wolvencraft/prison/mines/util/DrawingTools.java
+++ src/com/wolvencraft/prison/mines/util/DrawingTools.java
@@ -30,41 +30,4 @@ public enum DrawingTools {
 		}
 		return false;
 	}
-	
-	public static String toSmallFont(String input) {
-	    String returnString = "";
-	    boolean skip = false;
-	    boolean skipNext = false;
-	    for (int t = 0; t < input.length(); t++) {
-	      if (input.charAt(t) == '§') {
-	        skip = true;
-	        skipNext = true;
-	      }
-	      
-	      if (!skip) {
-	        if (input.charAt(t) == '/') {
-	          returnString = returnString + '╱';
-	        } else if (input.charAt(t) == ' ') {
-	          returnString = returnString + ' ';
-	        } else if (input.charAt(t) == '[') {
-	          returnString = returnString + '❲';
-	        } else if (input.charAt(t) == ']') {
-	          returnString = returnString + '❳';
-	        } else if (input.charAt(t) == '(') {
-	          returnString = returnString + '❲';
-	        } else if (input.charAt(t) == ')') {
-	          returnString = returnString + '❳';
-	        }
-	        else if ((input.charAt(t) <= '') && (input.charAt(t) >= ' ')) {
-	          returnString = returnString + (char)(65248 + input.charAt(t));
-	        } 
-	      } else {
-	        returnString = returnString + input.charAt(t);
-	        if (!skipNext) skip = false; else {
-	          skipNext = false;
-	        } 
-	      } 
-	    } 
-	    return returnString;
-	  } 
 }
