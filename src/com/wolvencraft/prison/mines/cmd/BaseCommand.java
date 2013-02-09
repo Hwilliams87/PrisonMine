0c2fcd5fdedd545932378546d786ffb35e9ff86f
Kirill Bondarchuk <bondarchuk.kirill@gmail.com>
2013-02-09 14:20:51 -0800

    Dev: Allow the CommandManager handle exceptions

--- src/com/wolvencraft/prison/mines/cmd/BaseCommand.java
+++ src/com/wolvencraft/prison/mines/cmd/BaseCommand.java
@@ -1,7 +1,7 @@
 package com.wolvencraft.prison.mines.cmd;
 
 public interface BaseCommand {
-	public boolean run(String[] args);
+	public boolean run(String[] args) throws Exception ;
 	public void getHelp();
 	public void getHelpLine();
 }
